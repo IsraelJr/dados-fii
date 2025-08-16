3ee8ce41fa8e13a4523b1f6b294be38c72ac6ad0
IsraelJr <israel.junior2111@gmail.com>
2025-08-16 16:50:48 -0300

    Teste inicial com nova index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-        <h1>📊 API de Fundos Imobiliários</h1>
+        <h1>📊 Dados de Fundos Imobiliários</h1>
         <p>Consulte informações resumidas de FIIs</p>
         <p>Exemplo de uso da API:</p>
         <code>/api/fii?ticker=MXRF11</code>
