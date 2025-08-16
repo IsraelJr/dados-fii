38538191666a04b85514223d69f0ad9e023e42d8
IsraelJr <israel.junior2111@gmail.com>
2025-08-16 17:16:04 -0300

    Teste inicial com nova index e criando API com pesquisa pelo Code.

--- src/app/pages/api/fii.ts
+++ src/app/pages/api/fii.ts
@@ -4,10 +4,11 @@ import admin from "firebase-admin";
 // Inicializa o Firebase Admin apenas uma vez
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
+    credential: admin.credential.cert(
+      JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)
+    ),
   });
 }
-
 
 const db = admin.firestore();
 
@@ -16,19 +17,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const { ticker } = req.query; // /api/fii?ticker=MXRF11
+    const { ticker } = req.query; // /api/fii?ticker=TGAR11
 
     if (!ticker || typeof ticker !== "string") {
       return res.status(400).json({ error: "Ticker é obrigatório" });
     }
 
-    const doc = await db.collection("Fiis").doc(ticker).get();
+    // Busca pelo campo 'Code'
+    const querySnapshot = await db
+      .collection("Fiis")
+      .where("Code", "==", ticker)
+      .limit(1)
+      .get();
 
-    if (!doc.exists) {
+    if (querySnapshot.empty) {
       return res.status(404).json({ error: "FII não encontrado" });
     }
 
-    res.status(200).json(doc.data());
+    const docData = querySnapshot.docs[0].data();
+
+    res.status(200).json(docData);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
