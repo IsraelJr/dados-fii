0eb747ce88db18203ba953496e6f75e7b4baf536
IsraelJr <israel.junior2111@gmail.com>
2025-09-22 01:47:48 -0300

    Ajustes no Google Ads.

---
+++ src/app/_document.tsx
@@ -0,0 +1,21 @@
+// app/_document.tsx ou pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                {/* Snippet obrigatório do Google AdSense */}
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3245357129779122"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
