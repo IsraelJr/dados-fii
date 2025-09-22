472d8b6eef684f8c9432123818968f5f1964f88a
IsraelJr <israel.junior2111@gmail.com>
2025-09-22 01:56:24 -0300

    Ajustes no Google Ads.

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
         <Html>
             <Head>
                 {/* Snippet obrigatório do Google AdSense */}
+                <meta name="google-adsense-account" content="ca-pub-3245357129779122"></meta>
                 <script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3245357129779122"
