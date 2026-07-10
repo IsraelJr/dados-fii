046aa6d3f52de49c28b2238c03f6cc675edbfce1
IsraelJr <32980364+IsraelJr@users.noreply.github.com>
2026-07-10 17:26:37 -0300

    Apply wallet page UX enhancer

--- src/app/carteira/layout.tsx
+++ src/app/carteira/layout.tsx
@@ -1,8 +1,10 @@
 import WalletEmailVerifiedSync from "../components/WalletEmailVerifiedSync";
+import WalletPageUxEnhancer from "../components/WalletPageUxEnhancer";
 
 export default function CarteiraLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full max-w-full overflow-x-hidden">
+      <WalletPageUxEnhancer />
       <div className="mx-auto w-full max-w-6xl px-4 pt-8">
         <WalletEmailVerifiedSync />
       </div>
