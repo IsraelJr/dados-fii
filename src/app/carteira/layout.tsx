77dcabecd2aadbe7ad83a7cc34f46d72dfa0685d
IsraelJr <32980364+IsraelJr@users.noreply.github.com>
2026-07-10 21:08:39 -0300

    Mount wallet historical summary enhancer

--- src/app/carteira/layout.tsx
+++ src/app/carteira/layout.tsx
@@ -1,10 +1,12 @@
 import WalletEmailVerifiedSync from "../components/WalletEmailVerifiedSync";
+import WalletHistoricalSummaryEnhancer from "../components/WalletHistoricalSummaryEnhancer";
 import WalletPageUxEnhancer from "../components/WalletPageUxEnhancer";
 
 export default function CarteiraLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full max-w-full overflow-x-hidden">
       <WalletPageUxEnhancer />
+      <WalletHistoricalSummaryEnhancer />
       <div className="mx-auto w-full max-w-6xl px-4 pt-8">
         <WalletEmailVerifiedSync />
       </div>
