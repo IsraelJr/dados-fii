eda88b64a8126d4c42eff6ec43258bc59076fe74
IsraelJr <32980364+IsraelJr@users.noreply.github.com>
2026-07-14 09:30:29 -0300

    refactor: export regulatory repository contract

--- src/services/regulatory/index.ts
+++ src/services/regulatory/index.ts
@@ -3,4 +3,5 @@ export * from "./RegulatoryDataService";
 export * from "./RegulatoryErrors";
 export * from "./RegulatoryNormalizer";
 export * from "./RegulatoryRepository";
+export * from "./RegulatoryRepositoryContract";
 export * from "./RegulatoryTypes";
